182e9be0fe28fbe1b1dfa2c6283172c036a182bc
Own3r <darkwebsite95@gmail.com>
2025-03-03 19:11:00 +0330

    Mobile menu will be closed after an item selected

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,5 @@
 "use client";
-import React from 'react'
+import React, { useState } from 'react'
 import useScreenScroll from '@/hooks/useScreenScroll';
 import { Menu, X } from 'lucide-react';
 import Image from 'next/image'
@@ -17,6 +17,9 @@ import {
 
 const Header = () => {
   const height = useScreenScroll();
+
+  const [isOpen, setIsOpen] = useState(false);
+  
 
   return (
     <header style={{height: height || 100}} className='header flex justify-between items-center fixed top-0 z-50 p-[0_50px] w-full bg-[#0a192fd9] cubicTransition'>
@@ -52,27 +55,27 @@ const Header = () => {
         </div>
 
         <div className="block md:hidden">
-          <Sheet>
+          <Sheet open={isOpen} onOpenChange={setIsOpen}>
             <SheetTrigger className='cursor-pointer'><Menu className='size-8 text-white' /></SheetTrigger>
-            <SheetContent className='bg-[#112240] flex justify-center items-center fixed top-0 right-0 bottom-0 p-[50px_10px] h-full'>
+            <SheetContent className='bg-[#112240] flex justify-center items-center fixed top-0 right-0 bottom-0 p-[50px_10px] h-full border-0'>
               <SheetHeader className='w-full'>
               <SheetTitle className='hidden'>Menu</SheetTitle>
                 <SheetDescription asChild>
                   <nav className='flex justify-between items-center w-full flex-col text-slate-400 text-center mobilelinks'>
 
                     <ol className='p-0 m-0 list-none w-full'>
-                      <li className='m-[0px_auto_10px] relative counter text-[13px]'>
+                      <li onClick={() => setIsOpen(false)} className='m-[0px_auto_10px] relative counter text-[13px]'>
                         <Link href="#about" className='p-[3px_20px_20px] w-full inline-block decoration-0 relative cubicTransition text-slate-300 hover:text-green'>About</Link>
                       </li>
-                      <li className='m-[0px_auto_10px] relative counter text-[13px]'>
+                      <li onClick={() => setIsOpen(false)} className='m-[0px_auto_10px] relative counter text-[13px]'>
                         <Link href="#projects" className='p-[3px_20px_20px] w-full inline-block decoration-0 relative cubicTransition text-slate-300 hover:text-green'>Projects</Link>
                       </li>
-                      <li className='m-[0px_auto_10px] relative counter text-[13px]'>
+                      <li onClick={() => setIsOpen(false)} className='m-[0px_auto_10px] relative counter text-[13px]'>
                         <Link href="#contact" className='p-[3px_20px_20px] w-full inline-block decoration-0 relative cubicTransition text-slate-300 hover:text-green'>Contact</Link>
                       </li>
                     </ol>
 
-                    <div className='hover:scale-105 w-full'>
+                    <div onClick={() => setIsOpen(false)} className='hover:scale-105 w-full'>
                       <Link className='flex justify-center items-center bg-transparent text-green border border-[#64ffda] rounded-md p-4 leading-[1] cubicTransition duration-1000 ml-[15px] text-[16px] resumeAnimation' href="#" target="_blank" rel="noopener noreferrer">Resume</Link>
                     </div>
 
