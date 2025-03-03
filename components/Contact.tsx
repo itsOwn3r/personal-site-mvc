b6e23f03cf3988624c3d3462571a5fcf9ac8e9b3
Own3r <darkwebsite95@gmail.com>
2025-03-03 19:25:44 +0330

    Loading state added to contact form

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -2,7 +2,7 @@
 import React, { FormEvent, useState } from 'react'
 import { Input } from "@/components/ui/input"
 import { Button } from './ui/button';
-import { Send } from 'lucide-react';
+import { LoaderIcon, Send } from 'lucide-react';
 import { isEmailValid } from '@/lib/isEmailValid';
 
 const Contact = () => {
@@ -15,6 +15,7 @@ const Contact = () => {
 
     const [error, setError] = useState("");
     const [isSuccessful, setIsSuccessful] = useState(false);
+    const [isLoading, setIsLoading] = useState(false);
 
 
 
@@ -22,32 +23,34 @@ const Contact = () => {
         e.preventDefault();
 
         setError("");
-
-        const validateEmail = isEmailValid(formData.email);
-
-        if (!validateEmail || formData.email.length < 10 || !formData.email.includes("@") || !formData.email.includes(".")) {
-            setError("Something is up with your email...");
-            return;
-        }
-
-        if (!formData.name || !formData.email || !formData.message) {
-            setError("You must enter all required inputs!");
-            return;
-        }
-
-        if (formData.name.length < 3) {
-            setError("Something is up with your name...");
-            return;
-        }
-
-
-        if (formData.message.length < 20) {
-            setError("Tell me more...");
-            return;
-        }
+        setIsLoading(true);
 
 
         try {
+
+            const validateEmail = isEmailValid(formData.email);
+
+            if (!validateEmail || formData.email.length < 10 || !formData.email.includes("@") || !formData.email.includes(".")) {
+                setError("Something is up with your email...");
+                return;
+            }
+
+            if (!formData.name || !formData.email || !formData.message) {
+                setError("You must enter all required inputs!");
+                return;
+            }
+
+            if (formData.name.length < 3) {
+                setError("Something is up with your name...");
+                return;
+            }
+
+
+            if (formData.message.length < 20) {
+                setError("Tell me more...");
+                return;
+            }
+
             const response = await fetch("/api/contact", {
                 method: "POST",
                 body: JSON.stringify(formData)
@@ -75,6 +78,8 @@ const Contact = () => {
         // eslint-disable-next-line @typescript-eslint/no-unused-vars
         } catch (error) {
             setError("Something went wrong, please try again later...");
+        } finally {
+            setIsLoading(false);
         }
 
 
@@ -86,27 +91,28 @@ const Contact = () => {
         <h3 className="title font-bold text-6xl my-3">Get In Touch</h3>
 
         {error && <h4 className="error text-red-500 font-bold text-2xl my-5">{error}</h4>}
-        {isSuccessful && <h4 className="error text-green font-bold text-2xl my-5">Your message has been sent to me!</h4>}
+        {isLoading && <h4 className="font-bold text-2xl mt-5 text-center w-full flex justify-center mb-8">{<LoaderIcon className='size-10 animate-spin' />}</h4>}
+        {isSuccessful && <h4 className="text-green font-bold text-2xl my-5">Your message has been sent to me!</h4>}
 
         <form onSubmit={handleSend}>
             <div className='my-4 text-center flex flex-col justify-center items-center'>
                 <span className='text-white'>Name:</span>
-                <Input name='name' onChange={(e) => setFormData((oldState) => ({...oldState, [e.target.name]: e.target.value}))} className='outline-0 border-green max-w-[90%] md:max-w-[50%] text-green py-6 px-4 placeholder:text-white/40' placeholder='Name' />
+                <Input disabled={isLoading} name='name' onChange={(e) => setFormData((oldState) => ({...oldState, [e.target.name]: e.target.value}))} className='outline-0 border-green max-w-[90%] md:max-w-[50%] text-green py-6 px-4 placeholder:text-white/40' placeholder='Name' />
             </div>
 
             <div className='my-4 text-center flex flex-col justify-center items-center'>
                 <span className='text-white'>Email:</span>
-                <Input name='email' onChange={(e) => setFormData((oldState) => ({...oldState, [e.target.name]: e.target.value}))} className='outline-0 border-green max-w-[90%] md:max-w-[50%] text-green py-6 px-4 placeholder:text-white/40' placeholder='Email' />
+                <Input disabled={isLoading} name='email' onChange={(e) => setFormData((oldState) => ({...oldState, [e.target.name]: e.target.value}))} className='outline-0 border-green max-w-[90%] md:max-w-[50%] text-green py-6 px-4 placeholder:text-white/40' placeholder='Email' />
             </div>
 
             <div className='mt-6 mb-4 text-center flex flex-col justify-center items-center'>
                 <span className='text-white'>Message:</span>
-                <textarea name='message' onChange={(e) => setFormData((oldState) => ({...oldState, [e.target.name]: e.target.value}))} placeholder='Write your message here...' className='outline-0 border-1 border-green max-w-[90%] md:max-w-[50%] text-green w-full rounded-xl p-[1rem_6rem_6rem_1rem]' />
+                <textarea disabled={isLoading} name='message' onChange={(e) => setFormData((oldState) => ({...oldState, [e.target.name]: e.target.value}))} placeholder='Write your message here...' className='outline-0 border-1 border-green max-w-[90%] md:max-w-[50%] text-green w-full rounded-xl p-[1rem_6rem_6rem_1rem]' />
             </div>
 
 
             <div className='hover:scale-115 mt-8 w-full flex justify-center'>
-                <Button  className='w-full max-w-[50%] md:max-w-[20%] bg-transparent hover:bg-transparent cursor-pointer text-green border border-green rounded-md p-[1.5rem_6rem] leading-[1.6] cubicTransition duration-1000 text-[19px] resumeAnimation'>Send <Send /></Button>
+                <Button disabled={isLoading} className='w-full max-w-[50%] md:max-w-[20%] bg-transparent hover:bg-transparent cursor-pointer text-green border border-green rounded-md p-[1.5rem_6rem] leading-[1.6] cubicTransition duration-1000 text-[19px] resumeAnimation'>Send <Send /></Button>
             </div>
 
         </form>
