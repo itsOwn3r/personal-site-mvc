c9e24a4dfbb71170f42ce0051cf89537679db589
Own3r <darkwebsite95@gmail.com>
2025-04-30 14:11:42 +0330

    new route added to master for contact form

---
+++ app/api/contact/other-project/route.ts
@@ -0,0 +1,53 @@
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+
+    try {
+        
+        const data = await req.json();
+
+        const { name, phone, message } = data;
+
+        if (!name || !phone || !message) {
+            return NextResponse.json({ success: false, message: "You must enter all required inputs!" });
+        }
+        
+        if (name.length < 3) {
+            return NextResponse.json({ success: false, message: "Something is up with your name..." });
+        }
+        
+        const isPhoneValid = new RegExp(/^09\d{9}$/).test(phone);
+        
+        if (!isPhoneValid) {
+            return NextResponse.json({ success: false, message: "Something is up with your Phone..." });
+        }
+
+
+        // Sending message to Telegram 
+
+        const botToken = process.env.botToken;
+        const chatId = process.env.chatId;
+
+        if (!botToken || !chatId) {
+            return NextResponse.json({ success: false, message: "Sending message to owner of this website is not configured yet!" })
+        }
+  
+        const request = await fetch(`https://api.telegram.org/bot${botToken}/sendMessage`, {
+            method: 'POST',
+            headers: {'Content-Type':'application/json'},
+                body: JSON.stringify({ chat_id: chatId, text: `New message from Contact Form of Personal Website with The Other Project! \n\n\nName: ${name}\n\nPhone: ${phone}\n\nMessage:\n ${message}` })
+            });
+    
+        const response = await request.json();
+
+        if (response.ok) {
+            return NextResponse.json({ success: true, message: "Sending message was successful!" })
+        } else {
+          return NextResponse.json({ success: false, message: "Sending message was not successful!" })
+        }
+        
+    } catch (error) {
+        console.log(error);
+        return NextResponse.json({ success: false, message: "Something went wrong!" });
+    }
+}
